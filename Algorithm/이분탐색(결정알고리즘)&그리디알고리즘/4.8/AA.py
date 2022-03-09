baa981887513c2f615021965cb20661f4c3f7eb0
kimseungkyu <kimseungkyubestiedu@gmail.com>
2022-03-09 20:59:23 +0900

    fix: 4.8 deque added

--- Algorithm/이분탐색(결정알고리즘)&그리디알고리즘/4.8/AA.py
+++ Algorithm/이분탐색(결정알고리즘)&그리디알고리즘/4.8/AA.py
@@ -1,9 +1,11 @@
 import sys
+from collections import deque
 
 sys.stdin = open("./input.txt", "r")
 n, limit = map(int, input().split())
 passenger = list(map(int, input().split()))
 passenger.sort()
+passenger = deque(passenger)
 cnt = 0
 while passenger:
     if len(passenger) == 1:
@@ -13,7 +15,7 @@ while passenger:
         passenger.pop()
         cnt += 1
     else:
-        passenger.pop(0)
+        passenger.popleft()
         passenger.pop()
         cnt += 1
 print(cnt)
