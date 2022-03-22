b21fb977836f8c7e7be6c48d03e928414b3262c8
kimseungkyu <kimseungkyubestiedu@gmail.com>
2022-03-22 09:37:24 +0900

    feat: chapter 5 stack review

---
+++ Algorithm/자료구조활용(스택,큐,해쉬,힙)/2.후위식 연산(스택_복습)/AA.py
@@ -0,0 +1,26 @@
+import sys
+
+sys.stdin = open("./input.txt", "rt")
+
+a = input()
+stack =[]
+for x in a:
+    if x.isdecimal():
+        stack.append(int(x))
+    elif x=='+':
+        n1 = stack.pop()
+        n2= stack.pop()
+        stack.append(n2+n1)
+    elif x=='-':
+        n1 = stack.pop()
+        n2= stack.pop()
+        stack.append(n2-n1)
+    elif x=='*':
+        n1 = stack.pop()
+        n2= stack.pop()
+        stack.append(n2*n1)
+    elif x=='/':
+        n1 = stack.pop()
+        n2= stack.pop()
+        stack.append(n2/n1)
+print(stack[0])
