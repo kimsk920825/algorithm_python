864bdfc9bf73cfa89dcae66822f6c98fd9bdfb93
kimseungkyu <kimseungkyubestiedu@gmail.com>
2022-03-24 00:43:44 +0900

    feat:6 완전탐색 복습~ 합이 같은 부분집합(DFS)

--- Algorithm/완전탐색(백트랙킹,상태트리와 Cut EDGE)-DFS(깊이우선탐색)기초/1.재귀함수를 이용한 이진수 출력/AA.py
+++ Algorithm/완전탐색(백트랙킹,상태트리와 Cut EDGE)-DFS(깊이우선탐색)기초/1.재귀함수를 이용한 이진수 출력/AA.py
@@ -2,13 +2,14 @@
 #return이라고 적으면 함수를 종료시켜라라는 뜻.
 import sys
 sys.stdin=open("input.txt","r")
+
 def DFS(x):
-    if x==0:
+    if x ==0:
         return
     else:
         DFS(x//2)
-        print(x%2, end='')
+        print(x%2, end=" ")
 
-if __name__=="__main__":
-    n = int(input())
+if __name__ == "__main__":
+    n=int(input())
     DFS(n)
